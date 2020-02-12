cca2a4faa44323a3f655835f0cb0fc84c801122c
rezamars <reza.mars@yahoo.com>
2020-02-12 10:13:53 +0100

    Done with MG choosing and making sure 5 MGs can be selected.

--- src/model/MGListener.java
+++ src/model/MGListener.java
@@ -13,6 +13,7 @@ import java.util.ResourceBundle;
 import javafx.application.Application;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -41,6 +42,10 @@ public class MGListener implements ActionListener   {
     private ImageView imageView1;
     private ImageView imageView2;
     
+    private int numberOfMGs = 0;
+    private int[] chosenMGIndexes = new int[5];
+    
+    
     public MGListener(Label[] MGA, Left left){
         
         for (int a = 0 ; a < mgFlagArray.length ; a++){
@@ -52,14 +57,12 @@ public class MGListener implements ActionListener   {
         this.imageViewArray = left.getImageViewArray();
         this.imageView1 = left.getImageView1();
         this.imageView2 = left.getImageView2();
-                
+        this.imageViewArray = left.getImageViewArray();
         
         /*
-        imageView1.setFitHeight(30);
-        imageView1.setFitWidth(30);
-        
-        imageView2.setFitHeight(30);
-        imageView2.setFitWidth(30);
+        for (int i = 0 ; i < MGchosenIndexes.length ; i++){
+            MGchosenIndexes[i] = -1;
+        }
         */
         
         loadLabelImage();
@@ -97,20 +100,50 @@ public class MGListener implements ActionListener   {
                     
                     if(event.getSource()== MGArray[0]){
                         MGIndex = 0;
-                        if (mgFlagArray[MGIndex] == false){
-                            mgFlagArray[MGIndex] = true;
-                            System.out.println("MGArray!: " + MGIndex + ", now true!");
-                        }
-                        else if(mgFlagArray[MGIndex] == true){
-                            mgFlagArray[MGIndex] = false;
-                            System.out.println("MGArray!: " + MGIndex + ", now false");
-                        }
-                        updateLabelImage(MGIndex);
                     }
-                    if(event.getSource()== MGArray[1]){
+                    else if(event.getSource()== MGArray[1]){
                         MGIndex = 1;
-                        System.out.println("MGArray!: " + MGIndex );
                     }
+                    else if(event.getSource()== MGArray[2]){
+                        MGIndex = 2;
+                    }
+                    else if(event.getSource()== MGArray[3]){
+                        MGIndex = 3;
+                    }
+                    else if(event.getSource()== MGArray[4]){
+                        MGIndex = 4;
+                    }
+                    else if(event.getSource()== MGArray[5]){
+                        MGIndex = 5;
+                    }
+                    else if(event.getSource()== MGArray[6]){
+                        MGIndex = 6;
+                    }
+                    else if(event.getSource()== MGArray[7]){
+                        MGIndex = 7;
+                    }
+                    else if(event.getSource()== MGArray[8]){
+                        MGIndex = 8;
+                    }
+                    else if(event.getSource()== MGArray[9]){
+                        MGIndex = 9;
+                    }
+                    else if(event.getSource()== MGArray[10]){
+                        MGIndex = 10;
+                    }
+                    else if(event.getSource()== MGArray[11]){
+                        MGIndex = 11;
+                    }
+                    else if(event.getSource()== MGArray[12]){
+                        MGIndex = 12;
+                    }
+                    else{
+                        System.exit(0);
+                    }
+                    
+                    MGLabelFlagSetter();
+                    updateLabelImage();
+                    //System.out.println("MGArray!: " + MGIndex );
                     
                 });
         }
@@ -131,16 +164,105 @@ public class MGListener implements ActionListener   {
         
     }
 
-    public void updateLabelImage(int labelIndex){
+    public void MGLabelFlagSetter(){
         
-        if (mgFlagArray[labelIndex] == false){
-            MGArray[labelIndex].setGraphic(imageView1);
+        if (mgFlagArray[MGIndex] == false){
+            mgFlagArray[MGIndex] = true;
+                System.out.println("MGArray!: " + MGIndex + ", now true!");
+            }
+        else if(mgFlagArray[MGIndex] == true){
+            mgFlagArray[MGIndex] = false;
+            System.out.println("MGArray!: " + MGIndex + ", now false");
         }
-        else if (mgFlagArray[labelIndex] == true){
-            MGArray[labelIndex].setGraphic(imageView2);
-        }
-                        
         
+    }
+    
+    public void updateLabelImage(){
+        
+       
+        
+        
+        
+        if (mgFlagArray[MGIndex] == false){
+                
+            if (numberOfMGs == 5){
+                for (int y = 0; y < MGArray.length ; y++){
+                    if (mgFlagArray[y] != true){
+                        MGArray[y].setDisable(false);
+                    }
+                    else {
+                        MGArray[y].setDisable(false);
+                    }
+                }
+            }
+                
+            imageViewArray[MGIndex].setImage(MGimage1);
+            MGArray[MGIndex].setGraphic(imageViewArray[MGIndex]);
+                /*
+                for (int x = 0 ; x < MGchosenIndexes.length ; x++){
+                    if (MGchosenIndexes[x] == MGIndex){
+                        MGchosenIndexes[x] = -1;
+                    }
+                }
+                */
+            if (numberOfMGs > 0){
+                numberOfMGs--;
+            }
+                
+            
+        }
+        else if (mgFlagArray[MGIndex] == true){
+                
+            imageViewArray[MGIndex].setImage(MGimage2);
+            MGArray[MGIndex].setGraphic(imageViewArray[MGIndex]);
+            //MGchosenIndexes[numberOfMGs] = MGIndex;
+            numberOfMGs++;
+        }
+            
+        
+        
+        /*for (int x = 0 ; x < MGchosenIndexes.length ; x++){
+            for (int q = 0 ; q < mgFlagArray.length ; q++){
+                if (mgFlagArray[q] == true){
+                    MGchosenIndexes[x] = q;
+                }
+                else {
+                    
+                }
+            }
+        }*/
+              
+        if (numberOfMGs > 4){
+            System.out.println("5+++++ " );
+            for (int y = 0; y < MGArray.length ; y++){
+                if (mgFlagArray[y] != true){
+                    MGArray[y].setDisable(true);
+                }
+                else {
+                    MGArray[y].setDisable(false);
+                }
+            }
+        }
+        
+         
+        int iter = 0;
+        
+        if (numberOfMGs == 5){
+            for (int y = 0; y < mgFlagArray.length ; y++){
+            if (mgFlagArray[y] == true){
+                chosenMGIndexes[iter] = y;
+                iter++;
+            }
+        }
+        }
+        
+        
+        
+        
+        System.out.println("number of MGs: " + numberOfMGs);
+        
+        System.out.println("Selected Indexes: " + chosenMGIndexes[0] + "," + chosenMGIndexes[1] + "," + chosenMGIndexes[2] + 
+                "," + chosenMGIndexes[3] + "," + chosenMGIndexes[4]);
     }
 
     
@@ -150,5 +272,6 @@ public class MGListener implements ActionListener   {
         System.out.println("ae");
         
     }
-
+    
+    
 }
